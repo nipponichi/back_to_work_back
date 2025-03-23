a86699e3ce9c3cb3760cbbbe6b7129439df53de1
nippinichi <javier.lopez.sola@gmail.com>
2025-03-23 12:53:46 +0100

    added add seeder

--- back_to_work_back/database/seeders/DatabaseSeeder.php
+++ back_to_work_back/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,7 +13,8 @@ class DatabaseSeeder extends Seeder
             //TeacherSeeder::class,
             //StudentSeeder::class,
             //SubjectTeacherClassroomSeeder::class,
-            UserSeeder::class
+            UserSeeder::class,
+            AddSeeder::class
         ]);
     }
 }
