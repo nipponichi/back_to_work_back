be1405bab4024a720b381331e3aa8a64b51711d9
ortu <jooror@hotmail.com>
2025-03-30 20:03:53 +0200

    cleaning code

--- back_to_work_back/routes/api.php
+++ back_to_work_back/routes/api.php
@@ -2,24 +2,13 @@
 
 use App\Http\Controllers\PassportLoginController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\SubjectController;
-use App\Http\Controllers\TeacherController;
-use App\Http\Controllers\ClassroomController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AdOfferController;
 use App\Http\Controllers\AdController;
 use App\Http\Controllers\AdChatController;
 use App\Http\Controllers\AdCategoryController;
 
-Route::prefix('categories')->group(function () {
-    Route::get('/', [AdCategoryController::class, 'index']); // Obtener todas las categorías
-    Route::post('/', [AdCategoryController::class, 'store']); // Crear nueva categoría
-    Route::get('{id}', [AdCategoryController::class, 'show']); // Obtener categoría por ID
-    Route::put('{id}', [AdCategoryController::class, 'update']); // Actualizar categoría
-    Route::delete('{id}', [AdCategoryController::class, 'destroy']); // Eliminar categoría
-});
-
+Route::apiResource('categories',AdCategoryController::class);
 Route::apiResource('offers', AdOfferController::class);
 Route::apiResource('ads', AdController::class);
 Route::get('chats', [AdChatController::class, 'index']); // Listar todos los chats
@@ -36,24 +25,7 @@ Route::middleware(['auth.validation2'])->group(function () {
 Route::middleware(['auth.validation'])->group(function(){
     
     Route::middleware(['id.validation'])->group(function () {
-        Route::get('/students/{id}', [StudentController::class, 'show']);
-        Route::put('/students/{id}', [StudentController::class, 'update']);
-        Route::delete('/students/{id}', [StudentController::class, 'destroy']);
     });
-
-    Route::apiResource('/students', StudentController::class)->except('show', 'update', 'destroy');
-    Route::get('/students/{id}/subject', [TeacherController::class, 'getSubjects']);
-
-    Route::apiResource('/subjects', SubjectController::class);
-    Route::get('/subjects/{id}/student', [SubjectController::class, 'getStudents']);
-    Route::get('/subjects/{id}/teacher', [SubjectController::class, 'getTeachers']);
-
-    Route::apiResource('/teachers', TeacherController::class);
-    Route::get('/teachers/{id}/subject', [TeacherController::class, 'getSubjects']);
-    Route::get('/teachers/{id}/classroom', [TeacherController::class, 'getClassrooms']);
-
-    Route::apiResource('/classrooms', ClassroomController::class);
-    Route::get('/classrooms/{id}/teacher', [ClassroomController::class, 'getTeachers']);
 
     Route::get('/userdata', [LoginController::class, 'userProfile']);
     Route::post('/logout', [LoginController::class, 'logout']);
